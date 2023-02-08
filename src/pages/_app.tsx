54fad8ab4fcbc44c3a31e5f9dce369670cecd530
HANYUNSEONG <hanyunseong.dev@gmail.com>
2023-02-09 00:25:37 +0900

    feat: nextseo 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Global } from "@emotion/react";
+import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import { globalStyle } from "theme/globalStyle";
 
@@ -6,6 +7,19 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Global styles={globalStyle} />
+      <DefaultSeo
+        title="멋지다 연진아~"
+        description="더 글로리 - 멋지다 연진아~ 생성기"
+        openGraph={{
+          type: "website",
+        }}
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: "/yeonjin.jpeg",
+          },
+        ]}
+      />
       <Component {...pageProps} />
     </>
   );
